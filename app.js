196cecb67dfd8ac02a4b09fddbc85dadc2ecc62c
ralph <alquisadaralph@gmail.com>
2020-09-07 15:23:59 +0800

    addeD

--- app.js
+++ app.js
@@ -70,6 +70,6 @@ app.use(function(err, req, res, next) {
 });
 
 connectDB();
-const server = app.listen(port, function() {
-	console.log('Server listening on port ' + port);
+const server = app.listen(port, function () {
+  console.log('Server listening on port ' + port);
 });
